6df007eccce52b059a2c378d4eca445fe3b0aabd
ofins <jkyeewang@gmail.com>
2025-05-10 14:47:17 +0800

    chore: add typescript

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
-const express = require("express");
+import express, { Request, Response } from "express";
+
+// const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
+app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
 
