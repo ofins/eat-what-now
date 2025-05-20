741f408c13edd42ffccc6f735cc97e3ee1a22763
ofins <jkyeewang@gmail.com>
2025-05-20 21:28:24 +0800

    feat: typeorm

---
+++ src/db/restaurant/data-source.ts
@@ -0,0 +1,15 @@
+import { DataSource } from 'typeorm';
+
+export const RestaurantDataSource = new DataSource({
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT || '5432'),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  synchronize: false,
+  logging: false,
+  entities: [__dirname + '/entities/*.ts'],
+  migrations: [__dirname + '/migrations/*.ts'],
+  subscribers: [],
+});
