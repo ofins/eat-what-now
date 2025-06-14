f4d249ea902d105aff0f4447a75048a62384f5b4
ofins <jkyeewang@gmail.com>
2025-06-14 12:36:44 +0800

    feat(home): setup location services

--- client/src/components/home/Home.tsx
+++ client/src/components/home/Home.tsx
@@ -1,8 +1,33 @@
 import { useEffect, useState } from "react";
 import Feed from "./Feed";
 
+interface ILocation {
+  latitude: number | null;
+  longitude: number | null;
+}
+
 const Home = () => {
   const [message, setMessage] = useState<string[]>([]);
+  const [location, setLocation] = useState<ILocation | null>(null);
+
+  useEffect(() => {
+    if (!navigator.geolocation) {
+      console.error("Geolocation is not supported by this browser.");
+      return;
+    }
+
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        setLocation({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+        });
+      },
+      (error) => {
+        console.error("Error getting location:", error);
+      }
+    );
+  }, []);
 
   useEffect(() => {
     const eventSource = new EventSource("http://localhost:3000/events");
@@ -25,6 +50,9 @@ const Home = () => {
   return (
     <div className=" w-full h-full flex flex-col justify-center items-center overflow-y-scroll">
       Home
+      <span>
+        Latitude: {location?.latitude}, Longitude: {location?.longitude}
+      </span>
       <span>Current time is {message.at(-1)}</span>
       <Feed />
     </div>
